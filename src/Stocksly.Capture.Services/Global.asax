d3560d7a08f76caa8a2376c05f20d6167f690927
Kosala Nuwan Perera <kosala.nuwan@gmail.com>
2015-11-08 12:00:40 +0530

    Initial commit for the domain.

---
+++ src/Stocksly.Capture.Services/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Stocksly.Capture.Services.Global" Language="C#" %>
---
+++ src/Stocksly.Model/Customers/ICustomersRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stocksly.Domain.Customers
+{
+    public interface ICustomersRepository
+    {
+    }
+}
